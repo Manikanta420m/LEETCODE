76c67dac6583e91b7b1029537ef5aa2ebdfbccad
ManiKanta <manikanta420m@gmail.com>
2025-03-09 09:16:14 +0530

    Time: 85 ms (12.97%) | Memory: 102.4 MB (99.37%) - LeetSync

--- 3483-alternating-groups-ii/alternating-groups-ii.cpp
+++ 3483-alternating-groups-ii/alternating-groups-ii.cpp
@@ -4,7 +4,7 @@ public:
         int n=colors.size();
         int ans=0,maxi=1;
         for(int i=1;i<n+k-1;i++){
-            if(colors[i%n]!=colors[(i-1+n)%n])maxi++;
+            if(colors[i%n]!=colors[(i-1)%n])maxi++;
             else maxi=1;
             if(maxi>=k)ans++;
         }return ans;
