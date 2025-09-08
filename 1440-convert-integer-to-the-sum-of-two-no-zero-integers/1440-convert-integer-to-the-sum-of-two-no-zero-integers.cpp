723d69b713f3b32934862f69ad71d5e79e22f80f
ManiKanta <manikanta420m@gmail.com>
2025-09-08 22:07:33 +0530

    Time: 0 ms (100%), Space: 8.1 MB (85.27%) - LeetHub

--- 1440-convert-integer-to-the-sum-of-two-no-zero-integers/1440-convert-integer-to-the-sum-of-two-no-zero-integers.cpp
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/1440-convert-integer-to-the-sum-of-two-no-zero-integers.cpp
@@ -1,17 +1,24 @@
 class Solution {
-private:
-    bool check(int n){
-        while(n>0){
-            if(n%10==0)return false;
-            n/=10;
-        }
-        return true;
-    }
 public:
     vector<int> getNoZeroIntegers(int n) {
-        for(int i=1;i<n;i++){
-            if(check(i) and check(n-i)) return {i,n-i};
+        int a=0,b=0,tens=1;
+        for(;n>0;n/=10,tens*=10){
+            int d=n%10;
+            if(d==0){
+                a+=5*tens;
+                b+=5*tens;
+                n-=10;
+            }
+            else if(d==1 and n>=10){
+                a+=6*tens;
+                b+=5*tens;
+                n-=10;
+            }
+            else{
+                a+=(d-d/2)*tens;
+                b+=(d/2)*tens;
+            }
         }
-        return {-1,-1};
+        return {a,b};
     }
 };
