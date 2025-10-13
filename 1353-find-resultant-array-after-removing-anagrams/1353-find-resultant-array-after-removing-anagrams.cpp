9436fdc792e29d45f74fcb1e4b6d4798f9d96155
ManiKanta <manikanta420m@gmail.com>
2025-10-13 06:25:14 +0530

    Time: 4 ms (32.66%), Space: 16.1 MB (54.91%) - LeetHub

---
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<string> removeAnagrams(vector<string>& words) {
+        vector<string>ans;
+        ans.push_back(words[0]);
+        for(int i=1;i<words.size();i++){
+           string word1=words[i];
+           sort(word1.begin(),word1.end());
+           string word2=words[i-1];
+           sort(word2.begin(),word2.end());
+           if(word1!=word2)ans.push_back(words[i]);
+        }
+        return ans;
+    }
+};
