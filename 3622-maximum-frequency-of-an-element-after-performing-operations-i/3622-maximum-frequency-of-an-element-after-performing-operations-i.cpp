6915db6b64a2262e9f684d8494e2cd271b772ff2
ManiKanta <manikanta420m@gmail.com>
2025-10-21 20:25:16 +0530

    Time: 367 ms (35.68%), Space: 149.3 MB (37.62%) - LeetHub

--- 3622-maximum-frequency-of-an-element-after-performing-operations-i/3622-maximum-frequency-of-an-element-after-performing-operations-i.cpp
+++ 3622-maximum-frequency-of-an-element-after-performing-operations-i/3622-maximum-frequency-of-an-element-after-performing-operations-i.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    static int maxFrequency(vector<int>& nums, int k, int numOperations) {
-        static constexpr int N=1e5+2;
-        int freq[N]={0}, sweep[N]={0}, mm=N, MM=0;
-        for(int x: nums){
-            freq[x]++;
-            const int x0=max(1, x-k), xN=min(x+k+1, N-1);
-            sweep[x0]++;
-            sweep[xN]--;
-            mm=min(mm, x0);
-            MM=max(MM, xN);
-        }
-        int ans=0, cnt=0;
-        for (int x=mm; x<=MM; x++){
-            cnt+=sweep[x];
-            ans=max(ans, freq[x]+min(cnt-freq[x], numOperations));
+    int maxFrequency(vector<int>& nums, int k, int numOperations) {
+        map<int, int> mp;
+        for (int num : nums) mp[num]++;
+        int freq = 0, ans = 0;
+        auto i = mp.begin(), j = mp.begin();
+        int minVal = mp.begin()->first, maxVal = prev(mp.end())->first;
+        for (int it = minVal; it <= maxVal; it++) {
+            while (j != mp.end() && j->first <= it + k) {
+                freq += j->second;
+                j++;
+            }
+            while (i != mp.end() && i->first < it - k) {
+                freq -= i->second;
+                i++;
+            }
+            ans = max(ans, min(mp[it] + numOperations, freq));
         }
         return ans;
     }
