c91b06339b4383dcd6d96edd592c4025f36bcffe
ManiKanta <manikanta420m@gmail.com>
2026-01-05 20:46:33 +0530

    Time: 4 ms (50.96%), Space: 9.2 MB (72.19%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,19 +1,17 @@
 class Solution {
-    int dp[201][201][201];
+    int dp[101][101];
 private:
     bool solve(int i,int j,int k,string &s1, string &s2, string &s3){
         if(k==0)return true;
-        if(dp[i][j][k]!=-1)return dp[i][j][k];
-        if(i>0 and j>0 and s1[i-1]==s2[j-1] and s1[i-1]==s3[k-1]){
-            return dp[i][j][k]=(solve(i-1,j,k-1,s1,s2,s3) or solve(i,j-1,k-1,s1,s2,s3));
+        if(dp[i][j]!=-1)return dp[i][j];
+        bool take1=false,take2=false;
+        if(i>0 and s1[i-1]==s3[k-1]){
+            take1=solve(i-1,j,k-1,s1,s2,s3);
         }
-        else if(i>0 and s1[i-1]==s3[k-1]){
-            return dp[i][j][k]=solve(i-1,j,k-1,s1,s2,s3);
+        if(j>0 and s2[j-1]==s3[k-1]){
+            take2=solve(i,j-1,k-1,s1,s2,s3);
         }
-        else if(j>0 and s2[j-1]==s3[k-1]){
-            return dp[i][j][k]=solve(i,j-1,k-1,s1,s2,s3);
-        }
-        else return dp[i][j][k]=false;
+        return dp[i][j]=take1 || take2;
     }
 public:
     bool isInterleave(string s1, string s2, string s3) {
