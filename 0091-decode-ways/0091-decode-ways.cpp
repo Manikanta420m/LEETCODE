148b04073a30d631f1e90c5366569727164f55e6
ManiKanta <manikanta420m@gmail.com>
2026-01-05 20:19:00 +0530

    Time: 0 ms (100%), Space: 8.5 MB (88.06%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int dp[105];
+    /*
     int solve(int idx,string s){
         if(idx==s.size())return 1;
         if(s[idx]=='0')return 0;
@@ -14,10 +15,19 @@ public:
         }
         return dp[idx]=one+two;
     }
+    */
     int numDecodings(string s) {
         if(s[0]=='0')return 0;
         int n=s.size();
-        memset(dp,-1,sizeof(dp));
-        return solve(0,s);
+        memset(dp,0,sizeof(dp));
+        dp[0]=1;
+        dp[1]=s[0]=='0'?0:1;
+        for(int i=2;i<=n;i++){
+            int one=s[i-1]-'0';
+            int two=(s[i-2]-'0')*10+(s[i-1]-'0');
+            if(one>=1)dp[i]+=dp[i-1];
+            if(two>=10 and two<=26)dp[i]+=dp[i-2];
+        }
+        return dp[n];
     }
 };
