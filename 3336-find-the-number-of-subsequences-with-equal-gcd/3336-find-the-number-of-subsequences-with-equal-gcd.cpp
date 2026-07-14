776c520770e9b8f38ff1646605cfa7d9288ffc8f
ManiKanta <manikanta420m@gmail.com>
2026-07-14 06:56:16 +0530

    Time: 399 ms (74.78%), Space: 290.7 MB (45.22%) - LeetHub

--- 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -1,26 +1,44 @@
 class Solution {
 public:
-    const int M=1e9+7;
+    static const int MOD = 1e9 + 7;
+
+    int n, mx;
+    vector<int> nums;
+    vector<vector<vector<int>>> dp;
+
+    int solve(int i, int g1, int g2) {
+        if (i == n)
+            return (g1 > 0 && g1 == g2);
+
+        int &ans = dp[i][g1][g2];
+        if (ans != -1) return ans;
+
+        long long res = 0;
+
+        // Skip
+        res += solve(i + 1, g1, g2);
+
+        // Put in seq1
+        int ng1 = (g1 == 0) ? nums[i] : gcd(g1, nums[i]);
+        res += solve(i + 1, ng1, g2);
+
+        // Put in seq2
+        int ng2 = (g2 == 0) ? nums[i] : gcd(g2, nums[i]);
+        res += solve(i + 1, g1, ng2);
+
+        return ans = res % MOD;
+    }
+
     int subsequencePairCount(vector<int>& nums) {
-        int m=*max_element(nums.begin(),nums.end());
-        vector<vector<int>>dp(m+1,vector<int>(m+1,0));
-        dp[0][0]=1;
-        for(auto i:nums){
-            auto ndp=dp;
-            for(int g1=0;g1<=m;g1++){
-                for(int g2=0;g2<=m;g2++){
-                   if(!dp[g1][g2])continue;
-                   int n1=gcd(i,g1),n2=gcd(i,g2);
-                   ndp[n1][g2]=(ndp[n1][g2]+dp[g1][g2])%M;
-                   ndp[g1][n2]=(ndp[g1][n2]+dp[g1][g2])%M;
-                }
-            }
-            dp=ndp;
-        }
-        int ans=0;
-        for(int i=1;i<=m;i++){
-            ans=(ans+dp[i][i])%M;
-        }
-        return ans;
+        this->nums = nums;
+        n = nums.size();
+
+        mx = *max_element(nums.begin(), nums.end());
+
+        dp.assign(n,
+                  vector<vector<int>>(mx + 1,
+                  vector<int>(mx + 1, -1)));
+
+        return solve(0, 0, 0);
     }
 };
