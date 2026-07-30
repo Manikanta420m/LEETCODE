460babc779b09ed1bd42930eedc0407f599c3155
ManiKanta <manikanta420m@gmail.com>
2026-07-30 13:05:14 +0530

    Time: 0 ms (100%), Space: 7.8 MB (91.78%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,18 +1,22 @@
 class Solution {
-private:
-    int solve(int n,vector<int>&dp){
-        if(n==0)return 1;
-        if(dp[n]!=-1)return dp[n];
-        int ans=1;
-        for(int i=1;i<=n;i++){
-            ans=max(ans,i*solve(n-i,dp));
-        }
-        return dp[n]=ans;
-    }
+// private:
+//     int solve(int n,vector<int>&dp){
+//         if(n==0)return 1;
+//         if(dp[n]!=-1)return dp[n];
+//         int ans=1;
+//         for(int i=1;i<=n;i++){
+//             ans=max(ans,i*solve(n-i,dp));
+//         }
+//         return dp[n]=ans;
+//     }
 public:
     int integerBreak(int n) {
         if(n<=3)return n-1;
-        vector<int>dp(n+1,-1);
-        return solve(n,dp);
+        int ans=1;
+        while(n>4){
+            ans*=3;
+            n-=3;
+        }
+        return ans*n;
     }
 };
