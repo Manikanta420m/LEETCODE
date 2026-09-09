12eac9f75f48f992df492ae8e456fd20c90e9a3a
ManiKanta <manikanta420m@gmail.com>
2026-09-09 13:11:04 +0530

    Time: 1086 ms (7.01%), Space: 0B (100%) - LeetHub

---
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -0,0 +1,14 @@
+SELECT s.user_id ,
+    ROUND(
+        AVG(
+           IF(c.action = 'confirmed',1.00,0)
+        ),
+        2
+    ) confirmation_rate
+FROM Signups s
+LEFT JOIN
+     Confirmations c
+     On 
+     s.user_id = c.user_id
+GROUP BY
+     s.user_id
